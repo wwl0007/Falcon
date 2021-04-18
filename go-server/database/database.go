ba15d1bf1b4668bc2d5353420092ff0d31d306d5
Bill Luu <billluu619@gmail.com>
2021-04-18 13:03:05 -0600

    Database now adds new patient data

--- go-server/database/database.go
+++ go-server/database/database.go
@@ -1,20 +1,28 @@
 package database
 
 import (
+	"log"
+
 	"github.com/wwl0007/Project3/models"
 	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
 )
 
 func New(dbFilename string) *gorm.DB {
-	// Hello world, the web server
 	db, err := gorm.Open(sqlite.Open(dbFilename), &gorm.Config{})
 	if err != nil {
 		panic("failed to connect database")
 	}
 
 	// Migrate the schema
-	db.AutoMigrate(&models.PatientData{}, &models.RelativeHistory{})
+	db.AutoMigrate(&models.PatientData{})
 
 	return db
+}
+
+func AddPatientData(db *gorm.DB, toAdd *models.PatientData) {
+	err := db.Create(toAdd).Error
+	if err != nil {
+		log.Printf("%+v", err)
+	}
 }
